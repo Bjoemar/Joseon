bbdae523d2a83b3bb3adc207d1a48b2039d05cd9
bjoemar <bacleaanjoemar22@gmail.com>
2019-07-12 19:19:55 +0800

    fix the web

--- routes/index.js
+++ routes/index.js
@@ -308,7 +308,7 @@ router.post('/saveLocation', urlencodedParser, function(request,response){
       fs.rename(agent_pic,newpath,function(err){
         if (err) throw err;
         response.write('FILES UPLOAD AND MOVED');
-        // response.end();
+        response.end();
       });
     })
 
