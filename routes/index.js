68fcbce00b3f15b75d805ebd6339fa3d887ef569
bjoemar <bacleaanjoemar22@gmail.com>
2019-07-12 12:41:04 +0800

    fix the upload picture

--- routes/index.js
+++ routes/index.js
@@ -307,12 +307,12 @@ router.post('/saveLocation', urlencodedParser, function(request,response){
 
       fs.rename(agent_pic,newpath,function(err){
         if (err) throw err;
-        // response.write('FILES UPLOAD AND MOVED');
-        // response.end();
+        response.write('FILES UPLOAD AND MOVED');
+        response.end();
       });
     })
 
-    response.redirect('/admin');
+    // response.redirect('/admin');
 });
 
 
